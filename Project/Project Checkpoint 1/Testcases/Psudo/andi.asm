c711bc4a02462cf6a975a5d592934dc71a467802
ellenchart <ellen.hart@richmond.edu>
2024-10-18 21:40:12 -0400

    andi test

---
+++ Project/Project Checkpoint 1/Testcases/Psudo/andi.asm
@@ -0,0 +1,21 @@
+.data
+.text
+.align 2
+.globl main 
+
+init:
+    # Test case: a = 12 (1100 in binary), immediate = 5 (0101 in binary)
+    addi $s0, $zero, 12  # a = 12
+
+main:
+    # Perform bitwise AND with immediate value 5, result should be 4 (0100 in binary)
+    andi $t0, $s0, 5
+
+    # Print the result
+    li $v0, 1
+    move $a0, $t0
+    syscall  # Should print 4
+
+    # Exit
+    li $v0, 10
+    syscall
