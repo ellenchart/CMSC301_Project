bee0f7d4a01adcaa50da72a521b0173f71065c2a
ellenchart <ellen.hart@richmond.edu>
2024-11-20 21:15:53 -0500

    soundTest

--- Project/Project_Checkpoint_4/SoundTest.asm
+++ Project/Project_Checkpoint_4/SoundTest.asm
@@ -5,10 +5,12 @@ main:
     addi $t0, $0, 67108715
     addi $t1, $0, 67108716
     addi $t2, $0, 67108717
-    addi $t3, $0, 400 # Set frequency to 440Hz
+    addi $t3, $0, 400 # set frequency to 440Hz
     sw $t3, 0($t0)           
     lw $t4, 0($t0)           
-    addi $t3, $0, 127
-    sw $t3, 0($t1)
-    lw $t4, 0($t1)           
-    j main                 
+    addi $t3, $0, 127 # set volume
+    sw $t3, 0($t2)
+    lw $t4, 0($t2)  
+    addi $t4, $0, 1 # enable buzzer
+    sw $t4, 0($t1)         
+    lw $t4, 0($t1)                   
