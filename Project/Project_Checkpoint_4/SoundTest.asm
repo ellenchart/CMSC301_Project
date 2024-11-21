5b3ce98f422f0df58c7b335f699a5740fc6a5d00
Dan Schwarz <dan.schwarz@outlook.com>
2024-11-20 21:18:10 -0500

    Commented SoundTest

--- Project/Project_Checkpoint_4/SoundTest.asm
+++ Project/Project_Checkpoint_4/SoundTest.asm
@@ -5,12 +5,11 @@ main:
     addi $t0, $0, 67108715
     addi $t1, $0, 67108716
     addi $t2, $0, 67108717
-    addi $t3, $0, 400 # set frequency to 440Hz
-    sw $t3, 0($t0)           
-    lw $t4, 0($t0)           
-    addi $t3, $0, 127 # set volume
-    sw $t3, 0($t2)
-    lw $t4, 0($t2)  
-    addi $t4, $0, 1 # enable buzzer
-    sw $t4, 0($t1)         
-    lw $t4, 0($t1)                   
+    addi $t3, $0, 400 # frequency value 400Hz
+    sw $t3, 0($t0) # set frequency      
+    lw $t4, 0($t0) # do nothing         
+    addi $t3, $0, 127 # volume value
+    sw $t3, 0($t2) # set volume
+    lw $t4, 0($t2) # do nothing  
+    sw $t4, 0($t1) # enable buzzer      
+    lw $t4, 0($t1) # do nothing                 
