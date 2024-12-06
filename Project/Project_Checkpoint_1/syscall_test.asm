0da929750da38b54315ebd5cbf9d4ba47182da48
ellenchart <ellen.hart@richmond.edu>
2024-12-06 16:34:36 -0500

    new

--- Project/Project_Checkpoint_1/syscall_test.asm
+++ Project/Project_Checkpoint_1/syscall_test.asm
@@ -1,47 +1,46 @@
 .data
-# Data for heap allocation (_syscall9) testing
-test_bytes: .word 20
-
-# Data for _syscall13 testing (e.g., a melody stored as frequency values)
-melody: .word 262, 294, 330, 349, 392, 440, 494, 523, 0  # Example melody (frequencies of notes in Hz)
+# Test data for syscalls
+heap_test: .word 128      # Number of bytes to allocate for syscall9
+char_to_print: .word 65   # ASCII 'A' for syscall11
+test_melody: .word 262, 294, 330, 349, 392, 440, 494, 523, 0  # Melody for syscall13
 
 .text
 main:
     # Test syscall 0: Initialization
-    addi $v0, $0, 0
+    addi $v0, $0, 0        # Load syscall code 0 (initialize)
     jal _syscallStart_
 
     # Test syscall 1: Print Integer
-    li $a0, -12345    # Load test integer into $a0
-    addi $v0, $0, 1   # Syscall code for _syscall1
+    addi $a0, $0, -12345   # Load test integer into $a0
+    addi $v0, $0, 1        # Load syscall code 1 (print integer)
     jal _syscallStart_
 
     # Test syscall 5: Read Integer
-    addi $v0, $0, 5   # Syscall code for _syscall5
+    addi $v0, $0, 5        # Load syscall code 5 (read integer)
     jal _syscallStart_
-    # Value read will be stored in $v0; you can use it for further tests.
+    # Check $v0 for the value read (may involve terminal input).
 
     # Test syscall 9: Heap Allocation
-    lw $a0, test_bytes  # Load number of bytes to allocate
-    addi $v0, $0, 9     # Syscall code for _syscall9
+    lw $a0, heap_test      # Load number of bytes to allocate from memory
+    addi $v0, $0, 9        # Load syscall code 9 (heap allocation)
     jal _syscallStart_
-    # Pointer to allocated heap block will be in $v0.
+    # $v0 will hold the heap pointer for the allocated block.
 
     # Test syscall 11: Print Character
-    li $a0, 65         # ASCII code for 'A'
-    addi $v0, $0, 11   # Syscall code for _syscall11
+    lw $a0, char_to_print  # Load ASCII character to print into $a0
+    addi $v0, $0, 11       # Load syscall code 11 (print character)
     jal _syscallStart_
 
     # Test syscall 12: Read Character
-    addi $v0, $0, 12   # Syscall code for _syscall12
+    addi $v0, $0, 12       # Load syscall code 12 (read character)
     jal _syscallStart_
-    # Character read will be stored in $v0.
+    # $v0 will hold the character read from input.
 
     # Test syscall 13: Play Melody
-    la $a0, melody     # Load address of the melody data
-    addi $v0, $0, 13   # Syscall code for _syscall13
+    la $a0, test_melody    # Load address of the melody into $a0
+    addi $v0, $0, 13       # Load syscall code 13 (play melody)
     jal _syscallStart_
 
-    # End Program
-    addi $v0, $0, 10   # Syscall code for _syscall10
+    # End program
+    addi $v0, $0, 10       # Load syscall code 10 (end program)
     jal _syscallStart_
