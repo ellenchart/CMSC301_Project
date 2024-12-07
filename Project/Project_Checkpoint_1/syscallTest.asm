86c99fbd6632a4b0b00a9ebad72c056b51f6f890
ellenchart <ellen.hart@richmond.edu>
2024-12-07 17:42:26 -0500

    small changes

--- Project/Project_Checkpoint_1/syscallTest.asm
+++
@@ -1,7 +0,0 @@
-.data
-.text
-main:
-addi $v0, $0, 5
-addi $a0, $0, 2
-syscall
-addi $t0, $0, 22
--- Project/Project_Checkpoint_1/syscall_test.asm
+++ Project/Project_Checkpoint_1/syscall_test.asm
@@ -1,57 +1,51 @@
 .data
-heap_test: .word 128      # Number of bytes to allocate for syscall9
-char_to_print: .word 65   # ASCII 'A' for syscall11
-test_melody: .word 262, 294, 330, 349, 392, 440, 494, 523, 0  # Melody for syscall13
+    _heap_test: .word 128
+    _char_to_print: .word 65
+    _test_melody: .word 262, 294, 330, 349, 392, 440, 494, 523, 0, 1
 
 .text
-
-# Entry point
-j main
-
-# Define label for equal case
-equal:
-    add $t4, $zero, $zero  # Reset $t4 to 0
-    j end                  # Jump to the end
+.globl main
 
 # Main logic
 main:
     # Test syscall 0: Initialization
     addi $v0, $zero, 0     # Load syscall code 0 (initialize)
-    jal _syscallStart_
+    syscall
 
     # Test syscall 1: Print Integer
     addi $a0, $zero, -12345   # Load test integer into $a0
     addi $v0, $zero, 1        # Load syscall code 1 (print integer)
-    jal _syscallStart_
+    syscall
 
     # Test syscall 5: Read Integer
     addi $v0, $zero, 5        # Load syscall code 5 (read integer)
-    jal _syscallStart_
+    syscall
     # Check $v0 for the value read.
+    add $t0, $v0, $zero
 
     # Test syscall 9: Heap Allocation
-    lw $a0, heap_test         # Load number of bytes to allocate
+    la $a0, _heap_test         # Load number of bytes to allocate
+    lw $a0, 0($t1)                     
     addi $v0, $zero, 9        # Load syscall code 9 (heap allocation)
-    jal _syscallStart_
+    syscall
+    add $t2, $v0, $zero
 
     # Test syscall 11: Print Character
-    lw $a0, char_to_print     # Load ASCII character into $a0
+    la $a0, _char_to_print     # Load ASCII character into $a0
+    lw $a0, 0($t3)                     
     addi $v0, $zero, 11       # Load syscall code 11 (print character)
-    jal _syscallStart_
+    syscall
 
     # Test syscall 12: Read Character
     addi $v0, $zero, 12       # Load syscall code 12 (read character)
-    jal _syscallStart_
+    syscall
+    add $t4, $v0, $zero
 
     # Test syscall 13: Play Melody
-    la $a0, test_melody       # Load address of the melody into $a0
+    la $a0, _test_melody       # Load address of the melody into $a0
     addi $v0, $zero, 13       # Load syscall code 13 (play melody)
-    jal _syscallStart_
+    syscall
 
     # End program
     addi $v0, $zero, 10       # Load syscall code 10 (end program)
     syscall                   # End program
-
-end:
-    addi $v0, $zero, 10       # Load syscall code 10 (end program)
-    syscall
