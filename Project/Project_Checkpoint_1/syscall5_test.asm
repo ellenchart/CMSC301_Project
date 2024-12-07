6bea0e7d81e3bd3b6f32987b5e8b0e1dab8eb196
ellenchart <ellen.hart@richmond.edu>
2024-12-07 18:49:26 -0500

    syscall 5 test

---
+++ Project/Project_Checkpoint_1/syscall5_test.asm
@@ -0,0 +1,12 @@
+.data
+.text
+.globl main
+
+# Main logic
+
+main:
+    # Test syscall 5: Read Integer
+    addi $v0, $zero, 5        # Load syscall code 5 (read integer)
+    syscall
+    # Check $v0 for the value read.
+    add $t0, $v0, $zero
