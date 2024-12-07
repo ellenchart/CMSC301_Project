4081d1f87875ca5ab694a831086e287badc8571f
ellenchart <ellen.hart@richmond.edu>
2024-12-07 18:36:02 -0500

    commit

--- Project/Project_Checkpoint_1/syscall1_test.asm
+++ Project/Project_Checkpoint_1/syscall1_test.asm
@@ -4,7 +4,10 @@
 
 # Main logic
 main:
-    # Test syscall 1: Print Integer
-    addi $a0, $zero, -12345   # Load test integer into $a0
     addi $v0, $zero, 1        # Load syscall code 1 (print integer)
+    # Test syscall 1: Print Integer
+    addi $a0, $zero, -12345   # Load test integer into $a0    
+    syscall
+end: 
+    addi $v0, $zero, 10
     syscall
