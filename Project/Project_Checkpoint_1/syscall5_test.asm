ff9379578056c695ddd8d23acb9c561db35b700e
ellenchart <ellen.hart@richmond.edu>
2024-12-08 15:40:44 -0500

    edits

--- Project/Project_Checkpoint_1/syscall5_test.asm
+++ Project/Project_Checkpoint_1/syscall5_test.asm
@@ -6,11 +6,8 @@
 
 main:
     # Test syscall 5: Read Integer
-    addi $a0, $0, 123
     addi $v0, $zero, 5        # Load syscall code 5 (read integer)
     syscall
-    # Check $v0 for the value read.
-    add $t0, $v0, $zero
 end: 
     addi $v0, $zero, 10
     syscall
