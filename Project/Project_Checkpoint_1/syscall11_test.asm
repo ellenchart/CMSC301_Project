63d917bae5adcf757c46cf10f81ebf13187e1db2
ellenchart <ellen.hart@richmond.edu>
2024-12-07 19:37:05 -0500

    syscall11

---
+++ Project/Project_Checkpoint_1/syscall11_test.asm
@@ -0,0 +1,15 @@
+.data
+.text
+.globl main
+
+# Main logic
+main:
+    # Test syscall 11: Print Character
+    la $a0, _char_to_print     # Load ASCII character into $a0
+    lw $a0, 0($t3)                     
+    addi $v0, $zero, 11       # Load syscall code 11 (print character)
+    syscall
+
+end: 
+    addi $v0, $zero, 10
+    syscall
